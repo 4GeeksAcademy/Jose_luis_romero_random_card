b95a672ad1600664ebb94b6966626eeb6f25a5c6
Jose Luis JoeDev <95258755+Jose890403@users.noreply.github.com>
2024-02-23 16:04:15 +0000

    primera prueba random cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let palos = ["♦", "♥", "♠", "♣️ "];
+let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let numeroCarta = document.getElementById("numero");
+let palosCarta = document.getElementById("palos");
+let palosCarta2 = document.getElementById("palos2");
+
+function generador(palos, numeros) {
+  let i = palos[Math.floor(Math.random() * palos.length)];
+  let j = numeros[Math.floor(Math.random() * numeros.length)];
+
+  palosCarta.textContent = i;
+  palosCarta2.textContent = i;
+  numeroCarta.textContent = j;
+}
+console.log(generador(palos, numeros));
+generador(palos, numeros);
